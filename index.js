17bac4b613e835f8fc66a29aaf816c2af8b0443b
Eli Fabrikant <pfabrikant@gmail.com>
2019-09-02 15:51:30 +0200

    A fix to a security problem. Still working on a bug in the logout functionality

--- index.js
+++ index.js
@@ -1,124 +1,152 @@
-const express = require('express');
-const {analyzeEntitySentimentOfText, analyzeSentimentOfText} = require ('./naturalLangApi');
+const express = require("express");
+const {
+    analyzeEntitySentimentOfText,
+    analyzeSentimentOfText
+} = require("./naturalLangApi");
 const app = express();
-const compression = require('compression');
-const bc = require('./src/lib/bcrypt');
-const db = require('./src/lib/dataBase');
-const bodyParser = require('body-parser');
-const cookieSession = require('cookie-session');
-const cookieSessionMiddleware= cookieSession({
-    secret: 'Death is part of life',
+const compression = require("compression");
+const bc = require("./src/lib/bcrypt");
+const db = require("./src/lib/dataBase");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+const cookieSessionMiddleware = cookieSession({
+    secret: "Death is part of life",
     maxAge: 1000 * 60 * 60 * 24 * 14
 });
-const {rephraseSentence} = require ('./src/lib/rephraser');
-const sanitizeHtml = require('sanitize-html');
-const cookieParser = require ('cookie-parser');
-const csurf = require ('csurf');
+const { rephraseSentence } = require("./src/lib/rephraser");
+const sanitizeHtml = require("sanitize-html");
+const cookieParser = require("cookie-parser");
+const csurf = require("csurf");
 app.use(cookieParser());
 app.use(cookieSessionMiddleware);
 app.use(bodyParser.json());
 app.use(compression());
 //csurf middleware
 app.use(csurf());
-app.use(function(req, res, next){
-    res.cookie('mytoken', req.csrfToken());
+app.use(function(req, res, next) {
+    res.cookie("mytoken", req.csrfToken());
     next();
 });
 
-
-
-if (process.env.NODE_ENV != 'production') {
+if (process.env.NODE_ENV != "production") {
     app.use(
-        '/bundle.js',
-        require('http-proxy-middleware')({
-            target: 'http://localhost:8081/'
+        "/bundle.js",
+        require("http-proxy-middleware")({
+            target: "http://localhost:8081/"
         })
     );
 } else {
-    app.use('/bundle.js', (req, res) => res.sendFile(`${__dirname}/bundle.js`));
+    app.use("/bundle.js", (req, res) => res.sendFile(`${__dirname}/bundle.js`));
 }
-app.use('/public/:fileName', (req,res)=>{
-    res.sendFile(__dirname+'/public/'+ req.params.fileName);
-
+app.use("/public/:fileName", (req, res) => {
+    res.sendFile(__dirname + "/public/" + req.params.fileName);
 });
 
-
-app.post('/submitText', (req,res)=>{
+app.post("/submitText", (req, res) => {
     var results;
     const clean = sanitizeHtml(req.body.text);
-    analyzeSentimentOfText(clean).then(data=>{
-        results=data;
-        if (req.session.loginId){
-            db.updateHistory(req.session.loginId,clean, Number(results.documentSentimentScore)).then(()=>{
-
-            }).catch(err=>console.log("Error in db.updatehistory in POST /submitText: ",err.message));
-        }
-        return analyzeEntitySentimentOfText(req.body.text);
-    }).then (moreResults=>{
-        results.entitiesAnalysis=moreResults;
-        res.json(results);
-    }).catch(err=>console.log("Error in POST /submitText: ",err.message));
+    analyzeSentimentOfText(clean)
+        .then(data => {
+            results = data;
+            if (req.session.loginId) {
+                db.updateHistory(
+                    req.session.loginId,
+                    clean,
+                    Number(results.documentSentimentScore)
+                )
+                    .then(() => {})
+                    .catch(err =>
+                        console.log(
+                            "Error in db.updatehistory in POST /submitText: ",
+                            err.message
+                        )
+                    );
+            }
+            return analyzeEntitySentimentOfText(req.body.text);
+        })
+        .then(moreResults => {
+            results.entitiesAnalysis = moreResults;
+            res.json(results);
+        })
+        .catch(err => console.log("Error in POST /submitText: ", err.message));
 });
-app.post('/getRephrasing', (req,res)=>{
-    rephraseSentence(req.body.text).then(results=>{
-        return analyzeSentimentOfText(results.join(' ').replace( /,/g,''));
-    }).then(data=>{
-        let list = data.sentences.filter(sentence=>Math.abs(sentence.sentenceSentiment)<Math.abs(req.body.score));
-        res.json(list);
-    }).catch(err=>console.log("Error in POST /getRephrasing: ", err.message));
+app.post("/getRephrasing", (req, res) => {
+    rephraseSentence(req.body.text)
+        .then(results => {
+            return analyzeSentimentOfText(results.join(" ").replace(/,/g, ""));
+        })
+        .then(data => {
+            let list = data.sentences.filter(
+                sentence =>
+                    Math.abs(sentence.sentenceSentiment) <
+                    Math.abs(req.body.score)
+            );
+            res.json(list);
+        })
+        .catch(err =>
+            console.log("Error in POST /getRephrasing: ", err.message)
+        );
 });
 
-app.post('/register', (req,res)=>{
-    bc.getHash(req.body.password).then((hash) => {
-        return db.insertUser(req.body.username, req.body.email, hash);
-    }).then(({rows}) => {
-        req.session.loginId = rows[0].id;
-        req.session.username= req.body.username;
-        res.json({loggedIn:true,
-            userId:rows[0].id });
-    }).catch(err=>{
-        console.log('error in POST /register: ', err.message);
-        res.json({loggedIn:false});
-    });
+app.post("/register", (req, res) => {
+    bc.getHash(req.body.password)
+        .then(hash => {
+            return db.insertUser(req.body.username, req.body.email, hash);
+        })
+        .then(({ rows }) => {
+            req.session.loginId = rows[0].id;
+            req.session.username = req.body.username;
+            res.json({ loggedIn: true, userId: rows[0].id });
+        })
+        .catch(err => {
+            console.log("error in POST /register: ", err.message);
+            res.json({ loggedIn: false });
+        });
 });
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
     let idInQuestion;
     console.log(req.body.username);
-    db.getPassword(req.body.username).then(({rows}) => {
-
-        idInQuestion = rows[0].id;
-        return bc.compareHash(req.body.password, rows[0].password);
-    }).then(() => {
-        req.session.loginId =idInQuestion;
-        req.session.username= req.body.username;
-        res.json({loggedIn:true,
-            userId:idInQuestion
+    db.getPassword(req.body.username)
+        .then(({ rows }) => {
+            idInQuestion = rows[0].id;
+            return bc.compareHash(req.body.password, rows[0].password);
+        })
+        .then(() => {
+            req.session.loginId = idInQuestion;
+            req.session.username = req.body.username;
+            res.json({ loggedIn: true, userId: idInQuestion });
+        })
+        .catch(err => {
+            console.log("Error in POST /login route: ", err.message);
+            res.json({ loggedIn: false });
         });
-
-    }).catch((err) => {
-        console.log('Error in POST /login route: ', err.message);
-        res.json({loggedIn:false});
-    });
 });
 
-app.get('/id', (req,res)=>{
-    if (req.session.loginId){
-        res.json({id:req.session.loginId, username:req.session.username});
+app.get("/logout", (req, res) => {
+    req.session = null;
+    console.log("cleared cookies on back-end: ", req.session);
+    res.json({ logout: true });
+});
+app.get("/id", (req, res) => {
+    if (req.session.loginId) {
+        res.json({ id: req.session.loginId, username: req.session.username });
     }
 });
 
-app.get('/getHistory/:id', (req,res)=>{
-    db.getHistory(req.params.id).then(({rows})=>{
-        res.json(rows);
-    }).catch(err=>console.log("Error in GET /getHistory: ", err.message));
+app.get("/getHistory/:id", (req, res) => {
+    db.getHistory(req.params.id)
+        .then(({ rows }) => {
+            res.json(rows);
+        })
+        .catch(err => console.log("Error in GET /getHistory: ", err.message));
 });
-app.get('/public/:filename', (req,res)=>{
-    res.sendFile(__dirname + '/public'+req.params.filename);
+app.get("/public/:filename", (req, res) => {
+    res.sendFile(__dirname + "/public" + req.params.filename);
 });
 
-app.get('*', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get("*", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(process.env.PORT || 8080, function() {
--- src/header.js
+++ src/header.js
@@ -1,26 +1,106 @@
-import React from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {logInId, updateHistory, updateUsername, } from './actions';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logInId, updateHistory, updateUsername } from "./actions";
+import { Link } from "react-router-dom";
+import instance from "./lib/axios";
 
-export function Header (params){
-    const loggedIn = useSelector (state=>state&&state.logInId);
+export function Header(params) {
+    const loggedIn = useSelector(state => state && state.logInId);
     const dispatch = useDispatch();
-    return (<div className="header">
-        <Link to="/"><div className="logo"><h2>senti<span>M</span>app</h2></div></Link>
-        <div className="navbar">
-            {!loggedIn&& <React.Fragment><Link to="/login"><h4 >login </h4></Link>
-                <Link to="/register"><h4> register </h4></Link></React.Fragment>
-            }
-            {params.match.url!='/'&&<Link to="/"><h4 > Sentiment Analysis </h4></Link>}
-            {params.match.url!='/about'&&<Link to="/about"><h4 >  About </h4></Link>}
-            {loggedIn&& <React.Fragment>
-                {params.match.url!='/history'&&<Link to="/history"><h4 > My History </h4></Link>}
-                <Link to="/"><h4 onClick={()=>{
-                    dispatch(logInId(null));
-                    dispatch(updateHistory(null));
-                    dispatch(updateUsername(null));
-                }}> logout</h4> </Link></React.Fragment>}
+    return (
+        <div className="header">
+            <Link to="/">
+                <div className="logo">
+                    <h2>
+                        senti<span>M</span>app
+                    </h2>
+                </div>
+            </Link>
+            <div className="navbar">
+                {!loggedIn && (
+                    <React.Fragment>
+                        <Link to="/login">
+                            <h4>login </h4>
+                        </Link>
+                        <Link to="/register">
+                            <h4> register </h4>
+                        </Link>
+                    </React.Fragment>
+                )}
+                {params.match.url != "/" && (
+                    <Link to="/">
+                        <h4> Sentiment Analysis </h4>
+                    </Link>
+                )}
+                {params.match.url != "/about" && (
+                    <Link to="/about">
+                        <h4> About </h4>
+                    </Link>
+                )}
+                {loggedIn && (
+                    <React.Fragment>
+                        {params.match.url != "/history" && (
+                            <Link to="/history">
+                                <h4> My History </h4>
+                            </Link>
+                        )}
+                        <Link to="/">
+                            <h4
+                                onClick={() => {
+                                    dispatch(logInId(null));
+                                    dispatch(updateHistory(null));
+                                    dispatch(updateUsername(null));
+                                    instance
+                                        .get("/logout")
+                                        .then(({ data }) => {
+                                            if (data.logout) {
+                                                // This function will attempt to remove a cookie from all paths.
+                                                var pathBits = location.pathname.split(
+                                                    "/"
+                                                );
+                                                var pathCurrent = " path=";
+
+                                                // do a simple pathless delete first.
+                                                document.cookie =
+                                                    "session= deleted ; expires=Thu, 01-Jan-1970 00:00:01 GMT;";
+
+                                                for (
+                                                    var i = 0;
+                                                    i < pathBits.length;
+                                                    i++
+                                                ) {
+                                                    pathCurrent +=
+                                                        (pathCurrent.substr(
+                                                            -1
+                                                        ) != "/"
+                                                            ? "/"
+                                                            : "") + pathBits[i];
+                                                    document.cookie =
+                                                        "session= deleted ; expires=Thu, 01-Jan-1970 00:00:01 GMT;" +
+                                                        pathCurrent +
+                                                        ";";
+                                                }
+                                            }
+                                            console.log(
+                                                "attempted to delete all cookies: ",
+                                                document.cookie
+                                            );
+                                        })
+                                        .catch(err =>
+                                            console.log(
+                                                "Error in logout route in Header component: ",
+                                                err.message
+                                            )
+                                        );
+                                }}
+                            >
+                                {" "}
+                                logout
+                            </h4>{" "}
+                        </Link>
+                    </React.Fragment>
+                )}
+            </div>
         </div>
-    </div>);
+    );
 }
